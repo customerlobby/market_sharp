3493e1d92ede294769a2e7bba3fa3f1fea2f9083
sreevs <sreevidya.allada@gmail.com>
2020-06-24 12:36:30 -0700

    PLAT-18 resolve gem dependencies

--- lib/market_sharp/connection.rb
+++ lib/market_sharp/connection.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module MarketSharp
   module Connection
     private
@@ -5,21 +7,21 @@ module MarketSharp
     def connection
       options = {
         verify_ssl: OpenSSL::SSL::VERIFY_NONE,
-        rest_options: {headers: { 'Content-Type': 'application/json', 'Authorization': "#{calculated_authorization}" } },
+        rest_options: { headers: { 'Content-Type': 'application/json', 'Authorization': calculated_authorization.to_s } }
       }
 
-      return @service ||= ::OData::Service.new(self.endpoint, options)
+      @service ||= ::OData::Service.new(endpoint, options)
     end
 
     def calculated_authorization
-       epoch_time_stamp = Time.now.utc.to_i
+      epoch_time_stamp = Time.now.utc.to_i
 
-       message = "#{self.company_id}#{self.user_key}#{epoch_time_stamp}"
+      message = "#{company_id}#{user_key}#{epoch_time_stamp}"
 
-       base64_secret_key = Base64.decode64(self.secret_key)
-       computed_secret = OpenSSL::HMAC.digest( 'sha256', base64_secret_key, message)
+      base64_secret_key = Base64.decode64(secret_key)
+      computed_secret = OpenSSL::HMAC.digest('sha256', base64_secret_key, message)
 
-       return "#{self.company_id}:#{self.user_key}:#{epoch_time_stamp}:#{Base64.encode64(computed_secret).strip}"
+      "#{company_id}:#{user_key}:#{epoch_time_stamp}:#{Base64.encode64(computed_secret).strip}"
     end
   end
 end
