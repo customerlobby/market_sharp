e9b2011bc2ff275dfc93525f9c9cbd9af3254694
sreevs <sreevidya.allada@gmail.com>
2020-07-15 16:47:01 -0700

    PLAT-18 temporary commit

--- spec/market_sharp/clients/customers_spec.rb
+++ spec/market_sharp/clients/customers_spec.rb
@@ -1,49 +1,52 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
+require 'spec_helper'
+require 'byebug'
 
 RSpec.describe MarketSharp::Client::Customers do
   pending "add some examples to (or delete) #{__FILE__}"
-  
-#   before do
-#     @client = MarketSharp::Client.new({company_id: 'abcd', user_key: '5766gy8ydjg', secret_key: 'jhgyytr4r66tyug86ruyg8'})
-#     @time_stamp = Time.now
-      
-#   end
 
-#   describe '#customers' do
+    before do
+      user_key =  '7181d828-df5b-406c-9768-f526507b6cd5'
+      secret_key = 'V0HzbUaTHeC2lhzt/2WE4Ox0oLf0sCVHPEXW4lTnYtk='
+      @client = MarketSharp::Client.new({company_id: '2685', user_key: user_key, secret_key: secret_key})
+      @time_stamp = Time.now
+    end
 
-#     before do
-#       @time_stamp = Time.now
-#       # @connection = OData::Service.new(@client.endpoint, {})
-#       # expect_any_instance_of(OData::Service).to receive(:new).and_return(connection)
-#       expect(Time).to receive(:now).and_return(@time_stamp)
-#       uri = URI.parse("https://api4.marketsharpm.com/WcfDataService.svc/$metadata?headers[Authorization]=#{calculated_authorization}")
-#       stub_request(:get, /https\:\/\/api4.marketsharpm.com\/WcfDataService.svc\/\$metadata*/ ).
-#         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'api4.marketsharpm.com', 'User-Agent'=>'rest-client/2.0.0 (darwin15.0.0 x86_64) ruby/2.3.0p0'}).
-#         to_return(:status => 200, :body => "#{fixture('customers.xml')}", :headers => {})
-#     end
+  #   describe '#customers' do
 
-#     it 'should return a list of customers' do
-#       expect_any_instance_of(OData::Service).to receive(:execute).with(anything, anything).and_return("#{fixture('customers_list.json')}")
-#       expect_any_instance_of(OData::Service).to receive(:set_namespaces).and_return(true)
-#       # expect_any_instance_of(OData::Service).to receive(:default_instance_vars).and_return(true)
-#       expect_any_instance_of(OData::Service).to receive(:build_classes_from_result).and_return(true)
-#       @client.customers({start_date: 10.days.ago, end_date: Date.current})
-#       # expect(a_get('https://api4.marketsharpm.com/WcfDataService.svc/$metadata?headers%5BAuthorization%5D=abcd:5766gy8ydjg:1478194988:u%2B9ygH8n1AbxcXSdC47AFakSSp68z5qFUpt9l7LNqmQ=').with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'api4.marketsharpm.com', 'User-Agent'=>'rest-client/2.0.0 (darwin15.0.0 x86_64) ruby/2.3.0p0'})).to have_been_made
-#     end
-#   end
+  #     before do
+  #       @time_stamp = Time.now
+  #       # @connection = OData::Service.new(@client.endpoint, {})
+  #       # expect_any_instance_of(OData::Service).to receive(:new).and_return(connection)
+  #       expect(Time).to receive(:now).and_return(@time_stamp)
+  #       uri = URI.parse("https://api4.marketsharpm.com/WcfDataService.svc/$metadata?headers[Authorization]=#{calculated_authorization}")
+  #       stub_request(:get, /https\:\/\/api4.marketsharpm.com\/WcfDataService.svc\/\$metadata*/ ).
+  #         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'api4.marketsharpm.com', 'User-Agent'=>'rest-client/2.0.0 (darwin15.0.0 x86_64) ruby/2.3.0p0'}).
+  #         to_return(:status => 200, :body => "#{fixture('customers.xml')}", :headers => {})
+  #     end
 
-#   def calculated_authorization
-#       epoch_time_stamp = @time_stamp.utc.to_i
+      it 'should return a list of customers' do
+  #       expect_any_instance_of(OData::Service).to receive(:execute).with(anything, anything).and_return("#{fixture('customers_list.json')}")
+  #       expect_any_instance_of(OData::Service).to receive(:set_namespaces).and_return(true)
+  #       # expect_any_instance_of(OData::Service).to receive(:default_instance_vars).and_return(true)
+  #       expect_any_instance_of(OData::Service).to receive(:build_classes_from_result).and_return(true)
+        customers = @client.customers({start_date: 10.years.ago, end_date: Date.current})
+        expect(customers[0].present?).to be_truthy
+  #       # expect(a_get('https://api4.marketsharpm.com/WcfDataService.svc/$metadata?headers%5BAuthorization%5D=abcd:5766gy8ydjg:1478194988:u%2B9ygH8n1AbxcXSdC47AFakSSp68z5qFUpt9l7LNqmQ=').with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'api4.marketsharpm.com', 'User-Agent'=>'rest-client/2.0.0 (darwin15.0.0 x86_64) ruby/2.3.0p0'})).to have_been_made
+      end
+  #   end
 
-#       message = "#{@client.company_id}#{@client.user_key}#{epoch_time_stamp}"
+  #   def calculated_authorization
+  #       epoch_time_stamp = @time_stamp.utc.to_i
 
-#       base64_secret_key = Base64.decode64(@client.secret_key)
-#       computed_secret = OpenSSL::HMAC.digest( 'sha256', base64_secret_key, message)
+  #       message = "#{@client.company_id}#{@client.user_key}#{epoch_time_stamp}"
 
-#       key = "#{@client.company_id}:#{@client.user_key}:#{epoch_time_stamp}:#{Base64.encode64(computed_secret).strip}"
-      
-#       return key.gsub('%20', '%2B')
-#     end
+  #       base64_secret_key = Base64.decode64(@client.secret_key)
+  #       computed_secret = OpenSSL::HMAC.digest( 'sha256', base64_secret_key, message)
 
+  #       key = "#{@client.company_id}:#{@client.user_key}:#{epoch_time_stamp}:#{Base64.encode64(computed_secret).strip}"
+
+  #       return key.gsub('%20', '%2B')
+  #     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'market_sharp'
-require 'webmock/rspec'
+# require 'webmock/rspec'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -18,12 +18,12 @@ RSpec.configure do |config|
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
-  config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
-    mocks.verify_partial_doubles = true
-  end
+  # config.mock_with :rspec do |mocks|
+  #   # Prevents you from mocking or stubbing a method that does not exist on
+  #   # a real object. This is generally recommended, and will default to
+  #   # `true` in RSpec 4.
+  #   mocks.verify_partial_doubles = true
+  # end
 
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
