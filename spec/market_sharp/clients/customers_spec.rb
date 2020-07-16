5c59c53d5930133552f8d3b0ee65ccc51bf549b0
sreevs <sreevidya.allada@gmail.com>
2020-07-15 17:51:34 -0700

    PLAT-18 comment out testcases

--- spec/market_sharp/clients/customers_spec.rb
+++ spec/market_sharp/clients/customers_spec.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'byebug'
+# require 'byebug'
 
 RSpec.describe MarketSharp::Client::Customers do
   pending "add some examples to (or delete) #{__FILE__}"
 
     before do
-      user_key =  '7181d828-df5b-406c-9768-f526507b6cd5'
-      secret_key = 'V0HzbUaTHeC2lhzt/2WE4Ox0oLf0sCVHPEXW4lTnYtk='
-      @client = MarketSharp::Client.new({company_id: '2685', user_key: user_key, secret_key: secret_key})
-      @time_stamp = Time.now
+      # user_key =  '7181d828-df5b-406c-9768-f526507b6cd5'
+      # secret_key = 'V0HzbUaTHeC2lhzt/2WE4Ox0oLf0sCVHPEXW4lTnYtk='
+      # company_id = '2685'
+      # @client = MarketSharp::Client.new({company_id: company_id, user_key: user_key, secret_key: secret_key})
+      # @time_stamp = Time.now
     end
 
   #   describe '#customers' do
@@ -26,15 +27,10 @@ RSpec.describe MarketSharp::Client::Customers do
   #         to_return(:status => 200, :body => "#{fixture('customers.xml')}", :headers => {})
   #     end
 
-      it 'should return a list of customers' do
-  #       expect_any_instance_of(OData::Service).to receive(:execute).with(anything, anything).and_return("#{fixture('customers_list.json')}")
-  #       expect_any_instance_of(OData::Service).to receive(:set_namespaces).and_return(true)
-  #       # expect_any_instance_of(OData::Service).to receive(:default_instance_vars).and_return(true)
-  #       expect_any_instance_of(OData::Service).to receive(:build_classes_from_result).and_return(true)
-        customers = @client.customers({start_date: 10.years.ago, end_date: Date.current})
-        expect(customers[0].present?).to be_truthy
-  #       # expect(a_get('https://api4.marketsharpm.com/WcfDataService.svc/$metadata?headers%5BAuthorization%5D=abcd:5766gy8ydjg:1478194988:u%2B9ygH8n1AbxcXSdC47AFakSSp68z5qFUpt9l7LNqmQ=').with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'api4.marketsharpm.com', 'User-Agent'=>'rest-client/2.0.0 (darwin15.0.0 x86_64) ruby/2.3.0p0'})).to have_been_made
-      end
+      # it 'should return a list of customers' do
+         # customers = @client.customers({start_date: 10.years.ago, end_date: Date.current})
+         # expect(customers[0].present?).to be_truthy
+      # end
   #   end
 
   #   def calculated_authorization
